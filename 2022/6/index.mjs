dd5f8d71f35f0e89605319041b39130296dd737b
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2022-12-14 23:04:20 +0000

    day 6 part 2

--- 2022/6/index.mjs
+++ 2022/6/index.mjs
@@ -3,13 +3,32 @@ import * as fs from 'fs/promises'
 const input = await fs.readFile('input.txt', { encoding: 'utf8' });
 const allChars = input.split('');
 
+// part 1
 for (let i = 0; i < allChars.length; i++) {
     if (i >= 3) {
         const lastFour = new Set([allChars[i-3],allChars[i-2],allChars[i-1],allChars[i]])
 
         if (lastFour.size === 4) {
             // unique!
-            console.log({answer: i + 1})
+            console.log({part1: i + 1})
+            break;
+        }
+    }
+}
+
+// part2
+const startOfMessageSize = 14;
+const startOfMessageSizeIndex = startOfMessageSize - 1;
+for (let i = 0; i < allChars.length; i++) {
+    if (i >= startOfMessageSizeIndex) {
+        const theSet = new Set();
+        for (let j = 0; j < startOfMessageSize; j++) {
+            theSet.add(allChars[i - j]);
+        }
+
+        if (theSet.size === startOfMessageSize) {
+            // unique!
+            console.log({part2: i + 1})
             break;
         }
     }

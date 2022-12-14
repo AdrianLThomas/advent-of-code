e64de6c9ad9a356103fd1582859e78ba02e9737b
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2022-12-14 22:03:23 +0000

    solution 5 part 2

--- 2022/5/index.mjs
+++ 2022/5/index.mjs
@@ -21,25 +21,40 @@ for (let i = 0; i < indexOfHeaders; i++) {
     }
 }
 
-const stringToInt = (x) => {
+const stringToIndex = (x) => {
     const num = parseInt(x, 10);
     if(Number.isInteger(num)) {
         return num;
     }
-    return x;
+    return [];
 }
 
 const allInstructions = inputArray.slice(indexOfHeaders + 2);
-for (let i = 0; i < allInstructions.length; i++) {
-    const [,times,,from,,to] = allInstructions[i].split(' ').map(stringToInt)
 
+const part1 = JSON.parse(JSON.stringify(stacks))
+const part2 = JSON.parse(JSON.stringify(stacks));
+for (let i = 0; i < allInstructions.length; i++) {
+    const [times,from,to] = allInstructions[i].split(' ').flatMap(stringToIndex)
+    const fromIndex = from - 1;
+    const toIndex = to - 1;
+
+    // part 1
     for (let j = 0; j < times; j++) {
-        stacks[to - 1].push(stacks[from - 1].pop());
+        part1[toIndex].push(part1[fromIndex].pop());
     }
+
+    // part 2
+    const fromStack = part2[fromIndex];
+    const toStack = part2[toIndex];
+    const crates = fromStack.splice(fromStack.length - times);
+    toStack.push(...crates);
 }
 
+const formatSolution = (stacks) => stacks.map(stack => stack[stack.length - 1])
+                                         .map(crate => crate.replace('[','').replace(']',''))
+                                         .join('')
+
 console.log({
-    topCrates: stacks.map(stack => stack[stack.length - 1])
-                     .map(crate => crate.replace('[','').replace(']',''))
-                     .join('')
+    part1: formatSolution(part1),
+    part2: formatSolution(part2),
 })

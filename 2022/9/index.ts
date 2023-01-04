a776500f9b4e137a2d38812deaee677484f14eca
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2023-01-04 19:19:00 +0000

    day 9 wip

--- 2022/9/index.ts
+++ 2022/9/index.ts
@@ -15,67 +15,86 @@ enum Direction {
 const ropeTolerance = 1;
 
 const isValidCoords = (head: Vector, tail: Vector): boolean => {
-    const diff: Vector = {x: Math.abs(head.x - tail.y), y: Math.abs(head.y - tail.y)};
+    const diff: Vector = {x: Math.abs(head.x - tail.x), y: Math.abs(head.y - tail.y)};
     
     return diff.x <= ropeTolerance && diff.y <= ropeTolerance;
 }
 
-const moveTail = (head: Vector, direction: Direction): Vector => {
-    const newTail: Vector = {...head};
+// const moveTail = (head: Vector, direction: Direction): Vector => {
+//     const newTail: Vector = {...head};
+//     switch (direction) {
+//         case Direction.U:
+//             newTail.y -= ropeTolerance;
+//             break;
+//         case Direction.R:
+//             newTail.x -= ropeTolerance;
+//             break;
+//         case Direction.D:
+//             newTail.y += ropeTolerance;
+//             break;
+//         case Direction.L:
+//             newTail.x += ropeTolerance;
+//             break;
+//     }
+
+//     return newTail;
+// }
+
+const moveHead = (head: Vector, direction: Direction): Vector => {
     switch (direction) {
         case Direction.U:
-            newTail.y -= ropeTolerance;
-            break;
+            return {...head, y: head.y + 1}
         case Direction.R:
-            newTail.x -= ropeTolerance;
-            break;
+            return {...head, x: head.x + 1}
         case Direction.D:
-            newTail.y += ropeTolerance;
-            break;
+            return {...head, y: head.y - 1}
         case Direction.L:
-            newTail.x += ropeTolerance;
-            break;
+            return {...head, x: head.x - 1}
     }
-
-    return newTail;
 }
 
-const head: Vector = {x: 0, y: 0};
-const tail: Vector = {x: 0, y: 0};
+const moveTail = (head: Vector, tail: Vector): Vector => {
+    const diff: Vector = {x: Math.abs(head.x - tail.x), y: Math.abs(head.y - tail.y)};
+    const xDiff = diff.x > 1;
+    const yDiff = diff.y > 1;
+    return {x: head.x - (tail.x + (xDiff ? ropeTolerance : 0)), y: head.y - (tail.y + (yDiff ? ropeTolerance : 0))};
+    // return {x: head.x / 2, y: head.y / 2};
+    // x diff, then +tolerance
+    // y diff, then +tolerance
+    
+    // head: x: 2, y: 0
+    // tail: x: 0, y, 0
+    // tail: x: 1, y, 0
+
+    // x: x - ropeTolerance
+    // y: y - ropeTolerance
+}
+
+let head: Vector = {x: 0, y: 0};
+let tail: Vector = {x: 0, y: 0};
 
 const allLines = input.split('\n');
-const tailPositions: Vector[] = [];
+let tailPositions = 1;
 for (const line of allLines) {
     const [directionString, timesString] = line.split(' ');
     const direction = directionString as Direction;
-    const times = +timesString;
+    const moves = +timesString;
 
     // move head
-    switch (direction) {
-        case Direction.U:
-            head.y += times;
-            break;
-        case Direction.R:
-            head.x += times;
-            break;
-        case Direction.D:
-            head.y -= times;
-            break;
-        case Direction.L:
-            head.x -= times;
-            break;
-    }
+    for (let i = 0; i < moves; i++) {
+        head = moveHead(head, direction);
 
-    // move tail
-    const validCoords = isValidCoords(head, tail);
-    if (!validCoords) {
-        const newPosition = moveTail(head, direction);
-        tail.x = newPosition.x;
-        tail.y = newPosition.y;
-        for (let i = 0; i < times - 1; i++) {
-            tailPositions.push(newPosition);
+        // move tail
+        const validCoords = isValidCoords(head, tail);
+        if (!validCoords) {
+            do {
+                tail = moveTail(head, tail);
+                tailPositions++;
+            } while (!isValidCoords(head, tail));
         }
     }
+
+
 }
 
-console.log({p1: tailPositions.length + 1})
+console.log({p1: tailPositions})

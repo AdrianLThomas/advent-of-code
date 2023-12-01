e1d23fa33fc48f4297835e915f43f342cf4c1abb
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2023-12-01 17:24:53 +0000

    feat: day 1 part 2, WIP solution (result different each time?)

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -6,17 +6,34 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	file, _ := os.Open("input.txt")
 	defer file.Close()
 
+	namedNumbers := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
 	digitRegexp := regexp.MustCompile(`\d`)
 	scanner := bufio.NewScanner(file)
 	count := 0
 	for scanner.Scan() {
-		digits := digitRegexp.FindAllString(scanner.Text(), -1)
+		thisLine := scanner.Text()
+		for namedNumber, digit := range namedNumbers {
+			thisLine = strings.Replace(thisLine, namedNumber, strconv.Itoa(digit), -1)
+		}
+		digits := digitRegexp.FindAllString(thisLine, -1)
 		digitLength := len(digits)
 		if digitLength > 0 {
 			firstDigit := digits[0]

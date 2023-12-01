b2e44801845dec78568acd9ef6b6aa002f7b9fe3
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2023-12-01 22:53:12 +0000

    fix: I think this will work when I fix the undeterministic loop

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -13,16 +13,16 @@ func main() {
 	file, _ := os.Open("input.txt")
 	defer file.Close()
 
-	namedNumbers := map[string]int{
-		"one":   1,
-		"two":   2,
-		"three": 3,
-		"four":  4,
-		"five":  5,
-		"six":   6,
-		"seven": 7,
-		"eight": 8,
-		"nine":  9,
+	namedNumbers := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
 	}
 
 	digitRegexp := regexp.MustCompile(`\d`)
@@ -31,7 +31,7 @@ func main() {
 	for scanner.Scan() {
 		thisLine := scanner.Text()
 		for namedNumber, digit := range namedNumbers {
-			thisLine = strings.Replace(thisLine, namedNumber, strconv.Itoa(digit), -1)
+			thisLine = strings.ReplaceAll(thisLine, namedNumber, digit+namedNumber)
 		}
 		digits := digitRegexp.FindAllString(thisLine, -1)
 		digitLength := len(digits)

c08866c587ea5fe83b23b6a7c6b89fb0ced9663e
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2025-12-02 22:32:49 +0000

    day 1 part 2, stumped, this should work

--- 2025/1/main.go
+++ 2025/1/main.go
@@ -8,6 +8,11 @@ import (
 )
 
 func main() {
+	p1()
+	p2()
+}
+
+func p1() {
 	const (
 		max       = 99
 		max_turns = max + 1
@@ -45,5 +50,45 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Score: %v \n", score)
+	fmt.Printf("Score - P1: %v \n", score)
+}
+
+func p2() {
+	const (
+		max       = 99
+		max_turns = max + 1
+		min       = 0
+		min_turns = min + 1
+		start     = 50
+	)
+	current := start
+	score := 0
+
+	file, _ := os.Open("./2025/1/input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		thisLine := scanner.Text()
+
+		direction := thisLine[:1]
+		amount, _ := strconv.Atoi(thisLine[1:])
+
+		if direction == "L" {
+			current = current - amount
+			for current < min {
+				current = max_turns + current
+				score = score + 1
+			}
+		} else { // R
+			current = current + amount
+			for current > max {
+				current = current - max_turns
+				score = score + 1
+			}
+		}
+	}
+
+	fmt.Printf("Score - P2: %v \n", score)
+	// 5829 too high
 }
